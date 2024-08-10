c6ddb9c740fa558df6592eb4d0a89d585b3ee024
Coogan, Andrew <acoogan@russellinvestments.com>
2024-08-09 17:49:46 -0700

    added test for the enumerable extensions.

---
+++ src/DoopExtensions/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace DoopExtensions.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static T SafeGetValue<T>(this IConfiguration configuration, string key, T defaultReturn = default)
+        {
+            try
+            {
+                var sectionExists = configuration.GetSection(key);
+                return sectionExists.Exists() ? configuration.StrictGetValue<T>(key) : defaultReturn;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentNullException($"Error in safely get config parameters: {ex.InnerException}");
+            }
+        }
+
+        public static T StrictGetValue<T>(this IConfiguration configuration, string key)
+        {
+            try
+            {
+                var sectionExists = configuration.GetSection(key);
+
+                if (sectionExists.Exists())
+                {
+                    return configuration.GetValue<T>(key);
+                }
+                else
+                {
+                    throw new ArgumentNullException($"{key} is not a valid key in the configuration file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentNullException($"Error in safely get config parameters for key {key}: {ex.InnerException}");
+            }
+        }
+
+        // string shortcuts as those are generally the most common
+        public static string StrictGetValue(this IConfiguration configuration, string key)
+        {
+            return configuration.StrictGetValue<string>(key);
+        }
+
+        public static string SafeGetValue(this IConfiguration configuration, string key)
+        {
+            return configuration.SafeGetValue(key, default(string));
+        }
+    }
+}
---
+++ src/DoopExtensions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks.Dataflow;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
+
+namespace DoopExtensions.Extensions
+{
+    internal static class EnumberableExtensions
+    {
+        public static async Task AsyncParallelForEach<T>(this IEnumerable<T> source, Func<T, Task> body, int maxDegreeOfParallelism = DataflowBlockOptions.Unbounded)
+        {
+            // this can not be a default parameter as it has to be compile time constant
+            if (maxDegreeOfParallelism == DataflowBlockOptions.Unbounded)
+            {
+                maxDegreeOfParallelism = Environment.ProcessorCount; // is this reasonable?
+            }
+
+            var tasks = new List<Task>();
+            using var semaphore = new SemaphoreSlim(maxDegreeOfParallelism);
+
+            foreach (var item in source)
+            {
+                await semaphore.WaitAsync();
+                try
+                {
+                    await body(item);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public static TOut GetCount<TOut>(this IEnumerable source) where TOut : struct, IComparable, IConvertible, IFormattable
+        {
+            var count = source.Cast<object>().Count();
+            var maxValue = Convert.ChangeType(typeof(TOut).GetField("MaxValue").GetValue(null), typeof(TOut));
+
+            if (Comparer<TOut>.Default.Compare((TOut)Convert.ChangeType(count, typeof(TOut)), (TOut)maxValue) > 0)
+            {
+                throw new OverflowException($"Count exceeds {typeof(TOut).Name}.MaxValue");
+            }
+
+            return (TOut)Convert.ChangeType(count, typeof(TOut));
+        }
+
+        public static T GetMostFrequentValue<T>(this IEnumerable<T> items)
+        {
+            var error = "Error in GetMostFrequentValue! ";
+            if (items?.Any() != true)
+            {
+                error += $"Submitted enumerable is empty.";
+                throw new ArgumentException(error);
+            }
+
+            if (items.Any(x => x == null))
+            {
+                error += $"Submitted enumerables contains null values.";
+                throw new ArgumentNullException(error);
+            }
+
+            var values = items.GroupBy(i => i).ToDictionary(k => k.Key, v => v.Count());
+            var highestFrequency = values.Max(i => i.Value);
+            var mostCommon = values.Where(v => v.Value.Equals(highestFrequency)).Select(k => k.Key);
+
+            if (mostCommon.Count() == 1)
+            {
+                return mostCommon.First();
+            }
+
+            // there are multiple most frequent, throw for now.
+            error += $"Multiple values tie for most frequent at {highestFrequency} repetitions: {mostCommon}.";
+            throw new IndexOutOfRangeException(error);
+        }
+
+        public static IEnumerable<IEnumerable<TIn>> ChunkBy<TIn>(this IEnumerable<TIn> source, int chunkSize)
+        {
+            while (source.Any())
+            {
+                yield return source.Take(chunkSize);
+                source = source.Skip(chunkSize);
+            }
+        }
+    }
+}
